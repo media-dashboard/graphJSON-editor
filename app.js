1bb9017032ea434172ca94391a9f5acd985bf562
James Conkling <james.lane.conkling@gmail.com>
2015-08-12 14:49:38 -0700

    create backbone app model

--- app.js
+++ app.js
@@ -6,62 +6,57 @@ var Graph = require('./models/graph');
 var Node = require('./models/node');
 var Edge = require('./models/edge');
 
+var App = Backbone.Model.extend({
+  initialize: function(){
+    this.classRoom = new Graph();
 
-// build classroom
-var raw = [
-  // [id, [lovers], [haters]]
-  { id: 1, lovers: [2], haters: [] },
-  { id: 2, lovers: [1], haters: [] },
-  { id: 3, lovers: [ ], haters: [1,4] },
-  { id: 4, lovers: [3], haters: [] },
-  { id: 5, lovers: [2], haters: [4] },
-  { id: 6, lovers: [2], haters: [] },
-];
+    rawStudents.map(function(rawStudent){
+      var classRoom = this.classRoom;
+      // create student, unless otherwise exists
+      var student = classRoom.hasNode(rawStudent.id) ? classRoom.getNode(rawStudent.id) : new Node({ id: rawStudent.id }).addToGraph(classRoom);
+      
+      rawStudent.lovers.forEach(function(loverId){
+        // get lover if exists, otherwise create
+        var lover = classRoom.hasNode(loverId) ? classRoom.getNode(loverId) : new Node({ id: loverId }).addToGraph(classRoom);
+        var edge = new Edge(student, lover, {attraction: 1}).addToGraph(classRoom);
+        student.addEdge(edge);
+      });
+      rawStudent.haters.forEach(function(haterId){
+        // get lover if exists, otherwise create
+        var hater = classRoom.hasNode(haterId) ? classRoom.getNode(haterId) : new Node({ id: haterId }).addToGraph(classRoom);
+        var edge = new Edge(student, hater, {attraction: -1}).addToGraph(classRoom);
+        student.addEdge(edge);
+      });
+    }.bind(this));
 
-var classRoom = new Graph();
+  },
 
-raw.map(function(rawStudent){
-  // create student, unless otherwise exists
-  var student = classRoom.hasNode(rawStudent.id) ? classRoom.getNode(rawStudent.id) : new Node({ id: rawStudent.id }).addToGraph(classRoom);
-  
-  rawStudent.lovers.forEach(function(loverId){
-    // get lover if exists, otherwise create
-    var lover = classRoom.hasNode(loverId) ? classRoom.getNode(loverId) : new Node({ id: loverId }).addToGraph(classRoom);
-    var edge = new Edge(this, lover, {attraction: 1}).addToGraph(classRoom);
-    student.addEdge(edge);
-  });
-  rawStudent.haters.forEach(function(haterId){
-    // get lover if exists, otherwise create
-    var hater = classRoom.hasNode(haterId) ? classRoom.getNode(haterId) : new Node({ id: haterId }).addToGraph(classRoom);
-    var edge = new Edge(this, hater, {attraction: -1}).addToGraph(classRoom);
-    student.addEdge(edge);
-  });
+  summarize: function(){
+    _.each(this.classRoom.getNodes(), function(student, id){
+      function getAttractions(student, attractionVal){
+        return _.chain(student.getEdges()).filter(function(edge){ 
+            return edge.get('attraction') === attractionVal;
+          }).map(function(edge){
+            return edge.getEndNode().get('id');
+          }).value();
+      }
+
+      var lovers = getAttractions(student, 1),
+          haters =  getAttractions(student, -1);
+
+      if(lovers.length > 0){
+        console.log("Student " + id + " loves " + lovers.join());
+      }else{
+        console.log("Student " + id + " is loveless");
+      }
+      if(haters.length > 0){
+        console.log("Student " + id + " hates " + haters.join());
+      }else{
+        console.log("Student " + id + " is free of hate");
+      }
+    });
+    
+  }
 });
 
-
-var summarize = function(){
-  _.chain(classRoom.getNodes()).each(function(student, id){
-    function getAttractions(student, attractionVal){
-      return _.chain(student.getEdges()).filter(function(edge){ 
-          return edge.get('attraction') === attractionVal;
-        }).map(function(edge){
-          return edge.getEndNode().get('id');
-        }).value();
-    }
-
-    var lovers = getAttractions(student, 1),
-        haters =  getAttractions(student, -1);
-
-    if(lovers.length > 0){
-      console.log("Student " + id + " loves " + lovers.join());
-    }else{
-      console.log("Student " + id + " is loveless");
-    }
-    if(haters.length > 0){
-      console.log("Student " + id + " hates " + haters.join());
-    }else{
-      console.log("Student " + id + " is free of hate");
-    }
-  });
-};
-summarize();
+window.app = new App();
