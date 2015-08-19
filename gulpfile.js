97232503ea068d9c55214b8b7c1bd99b9bb76035
James Conkling <james.lane.conkling@gmail.com>
2015-08-19 10:47:22 -0700

    add default gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ var b = watchify(browserify(opts));
 // add transformations here
 // i.e. b.transform(coffeeify);
 
-gulp.task('js', bundle); // so you can run `gulp js` to build the file
+gulp.task('default', bundle); // so you can run `gulp js` to build the file
 b.on('update', bundle); // on any dep update, runs the bundler
 b.on('log', gutil.log); // output build logs to terminal
 
