25674d4f4ef4fac17b26818f02b819eaa75071a6
James Conkling <james.lane.conkling@gmail.com>
2015-09-02 22:36:32 -0700

    add Handlebars templating

--- views/nodeLi.js
+++ views/nodeLi.js
@@ -1,6 +1,8 @@
 var Backbone = require('backbone');
 var _ = require('underscore');
 var d3 = require('d3');
+var Handlebars = require('handlebars');
+var $ = require('jquery');
 
 var NodeListItem = Backbone.View.extend({
   
@@ -30,7 +32,7 @@ var NodeListItem = Backbone.View.extend({
     this.listenTo(this.model, 'nodehoverleave', this.removeHighlightNodeLi)
   },
 
-  template: _.template('<span><%= id %></span> : <em><%= attr %></em>'),
+  template: Handlebars.compile( $('#nodeLi').html() ),
 
   highlightNodeLi: function(node){
     this.d3el.classed('hover', true);
@@ -40,7 +42,7 @@ var NodeListItem = Backbone.View.extend({
     this.d3el.classed('hover', false);
   },
 
-  render: function(){ 
+  render: function(){
     var listItem = this.template({ id: this.model.id, attr: this.model.get('name') });
     // TODO: replace d3el.html(...) w/ d3 templating fn
     this.d3el.html(listItem);
